d409ebddacc312f786e5ee9514abf49ca4b42a8a
unknown <ra194399@ACAD.UCDB.BR>
2024-11-07 17:39:17 -0400

    first-commit

---
+++ desafio_crud/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <!-- Barra -->
+    <v-app-bar color="primary">
+      <v-btn icon @click="drawer = !drawer">
+        <v-icon icon="mdi-menu"></v-icon>
+      </v-btn>
+
+
+      <v-app-bar-title>Cadastro de produtos </v-app-bar-title>
+    </v-app-bar>
+
+    <!-- Menu -->
+    <v-navigation-drawer color="primary" v-model="drawer">
+      <v-list-item to="/home" Link prepend-icon="mdi-home">
+        Home
+      </v-list-item>
+
+      <v-list-item to="/Produtos" Link prepend-icon="mdi-shopping">
+        Produtos
+      </v-list-item>
+
+      <v-list-item to="/Sair" Link prepend-icon="mdi-logout">
+        Sair
+      </v-list-item>
+
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const drawer = ref(false)
+</script>
---
+++ desafio_crud/src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Funcionando</h1>
+</template>
+
+<script>
+
+</script>
